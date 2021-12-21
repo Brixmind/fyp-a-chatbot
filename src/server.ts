65edb3db1d07db49770211a53057e5e71362924a
Reuben Wee <reuben.wee@gmail.com>
2021-12-21 09:00:38 +0800

    ss

--- src/server.ts
+++ src/server.ts
@@ -12,24 +12,10 @@ import { InitEvents, Middleware } from './middleware/default'
 console.log('hello world', process.env.BOT_TOKEN)
 
 
-// const userWizard = new Scenes.WizardScene('helpme-wizard',
-//     (ctx:any)=>{
-//         ctx.scene.session.user = {}
-//         ctx.reply('hi I\'m here to help, what can I do?')
-//         return ctx.wizard.next()
-//     },
-//     (ctx:any)=>{
-//         if (ctx.message.text.length<1) {
-//             return (ctx.reply('please try again'))
-//         }
-//         return ctx.scene.leave()
-//     }
-// )
-
 
 DBConnect().then((connection:any)=>{
     let bot = new Telegraf(process.env.BOT_TOKEN as string)
-    //InitEvents(bot)
+
     PluginInit(bot).then((stage:any)=>{
         //console.log('>>>>middleware',stage)
         bot.use(session()) // session is deprecated but it seems needed to set up stages...
